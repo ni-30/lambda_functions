32079c424ff820240a5b3059864f385555e53e14
Nitish Aryan <iitd.nitish@gmail.com>
2018-04-19 16:17:00 +0530

    Update AwsConfig.java

--- AlexaLambdaFunction/src/main/java/ntryn/alexa/AwsConfig.java
+++ AlexaLambdaFunction/src/main/java/ntryn/alexa/AwsConfig.java
@@ -3,9 +3,9 @@ package ntryn.alexa;
 import com.amazonaws.regions.Regions;
 
 public class AwsConfig {
-    public static final String accountId = "127887160317";
+    public static final String accountId = "";
     public static final Regions region = Regions.US_EAST_1;
-    public static final String gcmPlatformArn = "arn:aws:sns:us-east-1:127887160317:app/GCM/AlexaFusionPlay";
+    public static final String gcmPlatformArn = "";
     public static final String consoleWebUrl = "goo.gl/RVwSpK";
     public static final String GAME_CONTROL_TRIGGER_AUDIO = "https://s3.amazonaws.com/ask-soundlibrary/musical/amzn_sfx_bell_short_chime_01.mp3";
     public static final String GAME_CAROUSEL_TRIGGER_AUDIO = "https://s3.amazonaws.com/ask-soundlibrary/foley/amzn_sfx_glass_on_table_01.mp3";
